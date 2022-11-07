247404daa584bc287584baaa96b854043f932086
Yudianto Sujana <yudianto.sujana@staff.uns.ac.id>
2022-11-07 13:13:28 +0700

    First Commit

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({Key? key, required this.icon, required this.onPressed})
+      : super(key: key);
+
+  final IconData icon;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 0.0,
+      constraints: const BoxConstraints.tightFor(
+        width: 40.0,
+        height: 40.0,
+      ),
+      shape: const CircleBorder(),
+      fillColor: const Color(0xFF4C4F5E),
+      onPressed: onPressed,
+      child: Icon(icon),
+    );
+  }
+}
